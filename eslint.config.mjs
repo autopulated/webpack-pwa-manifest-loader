3c95ce4bc60b1424345a361608229db11723a3b2
James Crosby <james@coggle.it>
2024-07-29 15:33:14 +0100

    initial version

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+
+export default [
+  {files: ['**/*.js'], languageOptions: {sourceType: 'script'}},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+
+  {
+      rules: {
+        'no-trailing-spaces': 1,
+        'semi': 1,
+        'use-isnan': 1,
+        'valid-typeof': 1,
+        'arrow-spacing': [1, {'before':true, 'after':true}],
+        'func-call-spacing': [1, 'never'],
+        'semi-spacing': [1, {'before': false, 'after': true}],
+        'space-before-function-paren': [1, {'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always'}],
+        'brace-style': [1, '1tbs', {'allowSingleLine': true}],
+        'quotes': [1, 'single', { 'avoidEscape': true }]
+      }
+  }
+];
---
+++ index.mjs
@@ -0,0 +1,42 @@
+import path from 'node:path';
+
+// A simple loader to load image src URLs from manifest.json / .webmanifest files
+// Supports image src urls in:
+//  * .icons[].src
+//  * .screenshots[].src
+export default async function manifestLoader(source) {
+    const callback = this.async();
+
+    const parsedSrc = JSON.parse(source);
+
+    const logger = this.getLogger('pwa-manifest-loader');
+
+    const srcRefs = [];
+
+    // collect promises for the laod of each src url:
+    for (let i = 0; i < parsedSrc.icons?.length; i++) {
+        if (parsedSrc.icons[i]?.src) {
+            const promise = this.importModule(path.resolve(this.context, parsedSrc.icons[i].src)).catch((e) => {
+                logger.error(e);
+            });
+            srcRefs.push({promise, assignTo: parsedSrc.icons[i]});
+        }
+    }
+    for (let i = 0; i < parsedSrc.screenshots?.length; i++) {
+        if (parsedSrc.screenshots[i]?.src) {
+            const promise = this.importModule(path.resolve(this.context, parsedSrc.icons[i].src)).catch((e) => {
+                logger.error(e);
+            });
+            srcRefs.push({promise, assignTo: parsedSrc.screenshots[i]});
+        }
+    }
+
+    // await them each in turn and assign results:
+    for( const {promise, assignTo} of srcRefs) {
+        const result = await promise;
+        logger.log(`pwa-manifest-loader got ${result} for ${assignTo.src} in ${this.context}`);
+        assignTo.src = result;
+    }
+
+    callback(null, JSON.stringify(parsedSrc));
+};
